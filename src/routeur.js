1ead910e2181414bd9ee82b570803809e90a32ca
Stéphane <130861507+Stephanecns@users.noreply.github.com>
2023-08-03 23:58:18 +0200

    Etape 3 bis

--- src/routeur.js
+++ src/routeur.js
@@ -4,16 +4,23 @@ import Home from './pages/home';
 import About from './pages/about';
 import Housing from './pages/housing';
 import Error from './pages/404';
+import Header from './components/header'
+import Footer from './components/footer';
 
 const Router = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/housing" element={<Housing />} />
-      <Route path="*" element={<Error />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/housing" element={<Housing />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </>
   );
 };
+
 
 export default Router

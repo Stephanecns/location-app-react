187e8afb9f0f617112215523b1dbaedb8026d497
Stéphane <130861507+Stephanecns@users.noreply.github.com>
2023-08-07 11:30:35 +0200

    Réalisation du footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import LOGO from '../assets/logowhite.png';
+import '../styles/footer.scss'
 
 const Footer = () => {
   return (
-    <footer>
-      <p>Footer de l'application</p>
+    <footer className="footer">
+        <img src={LOGO} alt="Logo" className="footer__logo" />
+        <p className="footer__text">© 2020 Kasa. All rights reserved.</p>
     </footer>
   );
 };

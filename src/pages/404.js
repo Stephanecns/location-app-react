42db485efe02ea950561153d4f5c94177fdd2a25
Stéphane <130861507+Stephanecns@users.noreply.github.com>
2023-08-08 22:44:00 +0200

    Étape 6 : Créez la page d’erreur

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,29 @@
+
+import React from 'react';
+import { useNavigate } from 'react-router-dom'; // Importez useNavigate au lieu de useHistory
+import '../styles/error.scss';
+
 function Error() {
+    const navigate = useNavigate(); // Utilisez le hook useNavigate
+
+    const navigateToHome = () => {
+        navigate('/'); // Utilisez navigate pour aller à la page d'accueil
+    };
+
     return (
-        <div>
-            <h1>Error 🧮</h1>
+        <div className="not-found-container">
+            <div className="not-found-404">
+                404
+            </div>
+            <p className="not-found-text">
+                Oups! La page que vous demandez n'existe pas.
+            </p>
+            <p className="not-found-link" onClick={navigateToHome}>
+                Retourner sur la page d’accueil
+            </p>
         </div>
-    )
+    );
 }
-export default Error 
+
+export default Error;
+
